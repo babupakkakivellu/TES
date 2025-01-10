9b3d31f909a5716d50658d535a0f71e1d1473fdb
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2025-01-10 22:33:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    "api_id": "YOUR_API_ID",
-    "api_hash": "YOUR_API_HASH",
-    "bot_token": "YOUR_BOT_TOKEN",
-    "temp_dir": "temp_downloads"
+    "api_id": "16501053",
+    "api_hash": "d8c9b01c863dabacc484c2c06cdd0f6e",
+    "bot_token": "8125717355:AAGEqXec28WfZ5V_wb4bkKoSyTt_slw6x2I",
+    "temp_dir": "downloads"
 }
